3fbfd7c9f0d7a7a1156f2aa01dde5ad56143c71d
KevalSardhara <kevalsardhara9@gmail.com>
2024-05-01 16:12:15 +0530

    intial commit

--- routes/blogRouter.js
+++ routes/blogRouter.js
@@ -7,6 +7,54 @@ const path = require("path");
 
 const blogRouter = express.Router();
 
+
+// write the code in the utils folder
+const { v2: cloudinary } = require('cloudinary');
+const { fstat } = require('fs');
+
+cloudinary.config({
+    cloud_name: 'kevalsardhara',
+    api_key: '916462212994434',
+    api_secret: 'T8FWQBO8A9fGygJtfz8r4Q5kG78'
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            throw new Error("try again");
+        }
+        const responce = await cloudinary.uploader.upload(localFilePath,
+            { 
+                // public_id: "olympic_flag",
+                resource_type : 'auto'
+            },
+            function (error, result) {
+                if (error) {
+                    return res.status(400).json({
+                        status: false,
+                        message: error.message
+                    });
+                    // throw new Error("try again");
+                }
+                console.log(responce);
+            });
+        return responce;
+
+    } catch (error) {
+        // return next(error);
+        
+        fs.unlinkSync(localFilePath);
+        return res.status(400).json({
+            status: false,
+            message: error.message
+        });
+    }
+}
+
+// add the cloudenery with multer for the file upload
+// another option like the express-fileupload/multer
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, path.join(path.resolve(), './public/uploads/'));
@@ -42,12 +90,12 @@ blogRouter.post("/", upload.single('coverImageURL'), async function (req, res, n
 });
 
 blogRouter.get("/:id", async function (req, res, next) {
-    const blog = await Blog.findById({_id : req.params.id}).populate('createdBy');
-    const comment = await Comment.find({blogId : req.params.id}).populate('createdBy');
+    const blog = await Blog.findById({ _id: req.params.id }).populate('createdBy');
+    const comment = await Comment.find({ blogId: req.params.id }).populate('createdBy');
 
     console.log(blog);
     res.render('blog', {
-        user : req.user,
+        user: req.user,
         blog,
         comment,
     });
@@ -55,9 +103,9 @@ blogRouter.get("/:id", async function (req, res, next) {
 
 blogRouter.post("/comment/:blogId", async function (req, res, next) {
     const comment = await Comment.create({
-        comment : req.body.comment,
-        blogId : req.params.blogId,
-        createdBy : req.user._id,
+        comment: req.body.comment,
+        blogId: req.params.blogId,
+        createdBy: req.user._id,
     });
     return res.redirect(`/blog/${req.params.blogId}`);
 });
